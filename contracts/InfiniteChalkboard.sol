15ae01e2ed4b2112aaca3b2fbffd2b9e5729db56
Allen Lin <allenlin1992@hotmail.com>
2021-08-14 20:53:49 -0400

    add withdraw

--- contracts/InfiniteChalkboard.sol
+++ contracts/InfiniteChalkboard.sol
@@ -1,13 +1,15 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.7.0;
+pragma solidity ^0.7.3;
 
 contract InfiniteChalkboard {
     uint public cost = 0.1 ether;
     string public message;
     address payable public author;
+    address public owner;
 
     constructor() {
       author = msg.sender;
+      owner = msg.sender;
     }
 
     function write(string calldata _message) payable external {
@@ -17,5 +19,11 @@ contract InfiniteChalkboard {
         require(success, "Transfer to previous author failed.");
         author = msg.sender;
         cost += cost / 10;
+    }
+
+    function withdraw() external {
+      require(msg.sender == owner);
+      (bool success, ) = owner.call{value: address(this).balance}("");
+      require(success, "Withdraw failed.");
     }
 }
